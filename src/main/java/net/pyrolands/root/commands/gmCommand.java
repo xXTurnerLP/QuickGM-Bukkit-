3126c5f82ea74b42dcae19075911a9d93e2104a5
turner <xxturnerbgdani@gmail.com>
2019-09-02 23:40:17 +0300

    Fixed a type + Changed the command messages

--- src/main/java/net/pyrolands/root/commands/gmCommand.java
+++ src/main/java/net/pyrolands/root/commands/gmCommand.java
@@ -16,21 +16,21 @@ public class gmCommand implements CommandExecutor {
 
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (!(sender instanceof Player)) {
-			sender.sendMessage(ChatColor.AQUA + "Only players can use this command!");
+			sender.sendMessage(ChatColor.BLUE + "Error> " + ChatColor.GRAY + "Only players can execute this command.");
 			return true;
 		} else {
 			Player plr = (Player)sender;
 			if (plr.getGameMode() == GameMode.ADVENTURE || plr.getGameMode() == GameMode.SPECTATOR) {
-				sender.sendMessage(ChatColor.GOLD + "You have to be in either survival or creative to use this command");
+				sender.sendMessage(ChatColor.BLUE + "Error> " + ChatColor.GRAY + "You must be in either survival or creative to execute this command.");
 				return true;
 			} else {
 				if (plr.getGameMode() == GameMode.CREATIVE) {
 					plr.setGameMode(GameMode.SURVIVAL);
-					sender.sendMessage(ChatColor.GOLD + "Set game mode " + ChatColor.RED + "survival " + ChatColor.GOLD + "for " + ChatColor.DARK_RED + plr.getName());
+					sender.sendMessage(ChatColor.BLUE + "Moderation> " + ChatColor.GRAY + "Your game mode has been set to " + ChatColor.YELLOW + "survival" + ChatColor.GRAY + ".");
 					return true;
 				} else if (plr.getGameMode() == GameMode.SURVIVAL) {
 					plr.setGameMode(GameMode.CREATIVE);
-					sender.sendMessage(ChatColor.GOLD + "Set game mode " + ChatColor.RED + "creative " + ChatColor.GOLD + "for " + ChatColor.DARK_RED + plr.getName());
+					sender.sendMessage(ChatColor.BLUE + "Moderation> " + ChatColor.GRAY + "Your game mode has been set to " + ChatColor.YELLOW + "creative" + ChatColor.GRAY + ".");
 					return true;
 				}
 				return true;
